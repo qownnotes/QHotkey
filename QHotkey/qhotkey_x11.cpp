3040875161e6908c64d34a52832eb5f4c01d1751
Patrizio Bekerle <patrizio@bekerle.com>
2025-10-01 22:18:14 +0200

    fix: possible issue on Qt6
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- QHotkey/qhotkey_x11.cpp
+++ QHotkey/qhotkey_x11.cpp
@@ -132,8 +132,10 @@ quint32 QHotkeyPrivateX11::nativeKeycode(Qt::Key keycode, bool &ok)
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 2, 0)
 	const QNativeInterface::QX11Application *x11Interface = qGuiApp->nativeInterface<QNativeInterface::QX11Application>();
-    if (!x11Interface)
+    if (!x11Interface) {
         return 0;
+    }
+
 	Display *display = x11Interface->display();
 #else
 	const bool x11Interface = QX11Info::isPlatformX11();
